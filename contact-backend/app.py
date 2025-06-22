17f23579969aa45f5944f478318e9a4c65649c47
Priyotosh Roy <you@example.com>
2025-06-22 22:28:43 +0530

    Fix: Added host and port for Render

--- contact-backend/app.py
+++ contact-backend/app.py
@@ -31,4 +31,6 @@ def send_email():
         return jsonify({'status': 'error', 'message': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  
+    app.run(host='0.0.0.0', port=port, debug=True)
+
